73033db56a6208996ebb3875c6fc11e90e156aae
DDALGI <cherish0349@gamil.com>
2023-04-01 21:46:51 +0900

    Update21-03WithPayment

---
+++ class/23-01-point-transaction/backend/src/apis/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
+import { UserService } from '../users/user.service';
+import { JwtAccessStrategy } from 'src/commons/auth/jwt-access.strategy';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+    // register({}) : JWT 토큰 생성시 설정내용(만료 시간 등)
+    JwtModule.register({}), //
+    // user table 조회를 위해 TypeOrmModule 추가
+    TypeOrmModule.forFeature([User]),
+  ],
+  providers: [
+    AuthResolver, //
+    AuthService,
+    UserService,
+    JwtAccessStrategy,
+  ],
+  controllers: [
+    AuthController,
+    //
+  ],
+})
+export class AutuModule {}
