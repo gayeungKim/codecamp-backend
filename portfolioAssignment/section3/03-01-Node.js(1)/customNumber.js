9aec0d9e8c6291f373ba680660a8cc343ea42f19
DDALGI <cherish0349@gamil.com>
2023-04-07 13:39:36 +0900

    03-01-customResidentRegistrationNumber

---
+++ portfolioAssignment/section3/03-01-Node.js(1)/customNumber.js
@@ -0,0 +1,33 @@
+// 1 유효한 주민번호 확인
+export function isValidRegistrationNumber(registrationNumber){
+    // 받아온 인자 string으로 변환
+    let check = registrationNumber + "";
+    // division이 '-'인지 확인하기 위해 문자열에서 받아옴
+    let division = check.charAt(6);
+    // '-'이후 7자리인지 확인하기 위해 문자열에서 받아옴
+    let bNumber = registrationNumber.substr(7);
+
+    // division이 '-'이 아닌경우
+    if(division != '-'){
+        console.log('형식이 올바르지 않다');
+        return;
+    } else if(bNumber.length !=7){ // 뒷자리가 7자리가 아닌경우
+        console.log('뒷자리 숫자가 잘못됨');
+        return;
+    } else { // 형식이 올바르다면 return으로 주민번호 넘겨줌
+        // console.log('올바른 형식');
+        return check;
+    }
+
+}
+
+// 2 주민번호 암호화
+// isValidNuber에서 넘겨받은 주민번호 암호화
+export function custom(validNumber){
+    let customNumber = validNumber + "";
+    let customedNumber = customNumber.substr(0,8) + "******";
+
+    // 암호화된 주민번호 출력하고 return
+    console.log("암호화된 주민번호: "+customedNumber);
+    return customedNumber;
+}
---
+++ portfolioAssignment/section3/03-01-Node.js(1)/resident-registration-number.js
@@ -0,0 +1,22 @@
+// 1 주민번호 만들기
+// 뒷자리를 가리는 함수(customRegistrationNumber)를 하나 만들고 해당 함수에
+// 주민번호를 넣었을 경우 암호화해서 출력할 것
+
+// 조건: 퍼사드패턴 사용!
+import { isValidRegistrationNumber, custom } from "./customNumber.js";
+
+function customRegistrationNumber(inputregistrationNumber){
+    // 1 유효한 주민번호인지 확인: "6자리숫자" + "-" + "7자리숫자"
+    const validNumber = isValidRegistrationNumber(inputregistrationNumber);
+
+    // 2 주민번호 암호화
+    if(!validNumber){ // return 값이 없다면
+        return; // validNumber 함수를 실행하지 않을 것
+    } else { // return 값이 있다면
+        custom(validNumber); // validNumber 함수를 실행할 것
+    }
+}
+
+customRegistrationNumber('000000-0000000'); // 형식이 올바를때
+customRegistrationNumber('000000-000000000000'); // 숫자가 넘었을 때
+customRegistrationNumber('0000000000000'); // 형식이 올바르지 않을 때
